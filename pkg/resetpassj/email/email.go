66d69f1e67ed7b2a2078cafe31e0e0c099b7c174
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-11-10 13:06:08 -0500

    update email

--- pkg/resetpassj/email/email.go
+++ pkg/resetpassj/email/email.go
@@ -7,19 +7,32 @@ import (
 
 type EmailService struct {
 	dialer *gomail.Dialer
-	user   string
+	sender string
 }
 
-func NewEmailService(user, pass string) *EmailService {
-	dialer := gomail.NewDialer("smtp.gmail.com", 587, user, pass)
-	return &EmailService{dialer: dialer, user: user}
+type SMTPConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+// NewEmailService permite configurar cualquier proveedor SMTP (Gmail, Outlook, Mailgun, etc.)
+func NewEmailService(cfg SMTPConfig) *EmailService {
+	dialer := gomail.NewDialer(cfg.Host, cfg.Port, cfg.Username, cfg.Password)
+
+	return &EmailService{
+		dialer: dialer,
+		sender: cfg.Username,
+	}
 }
 
 func (e *EmailService) Send(to, subject, body string) error {
 	msg := gomail.NewMessage()
-	msg.SetHeader("From", e.user)
+	msg.SetHeader("From", e.sender)
 	msg.SetHeader("To", to)
 	msg.SetHeader("Subject", subject)
 	msg.SetBody("text/html", body)
+
 	return e.dialer.DialAndSend(msg)
 }
