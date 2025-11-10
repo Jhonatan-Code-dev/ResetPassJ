ab54293c81a05584d849a7dcb84b349198cef879
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-11-09 21:17:21 -0500

    update gitinore

--- pkg/resetpassj/logic/logic.go
+++ pkg/resetpassj/logic/logic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"ResetPassJ/pkg/resetpassj/email"
+	"ResetPassJ/pkg/resetpassj/storage"
+	"crypto/rand"
+	"fmt"
 	"time"
 )
 
@@ -16,4 +19,13 @@ func (rm *ResetManager) SendResetCode(to string) error {
 	rm.store.SaveCode(to, code, time.Now().Add(2*time.Minute))
 	body := "Tu código de restablecimiento es: " + code
 	return rm.email.Send(to, "Restablecimiento de contraseña", body)
+}
+
+func generateRandomCode() string {
+	b := make([]byte, 6)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "000000" // fallback
+	}
+	return fmt.Sprintf("%06d", int(b[0])%1000000)
 }
